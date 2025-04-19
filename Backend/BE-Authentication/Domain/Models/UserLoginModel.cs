977e2f8348169e450260c45a734eb6c07970c366
Mateus Henrique <akira.dev.math@gmail.com>
2025-04-19 15:34:21 -0300

    feat: frontend docker config, auth controller creation, user register and login models creation

---
+++ Backend/BE-Authentication/Domain/Models/UserLoginModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Domain.DTO;
+
+public class UserLoginModel
+{
+    [Required(ErrorMessage = "The field {0} is required!")]
+    [EmailAddress(ErrorMessage = "The field {0} is not a valid format!")]
+    public required string Email { get; set; }
+
+    [Required(ErrorMessage = "The field {0} is required!")]
+    [StringLength(100, ErrorMessage = "The field {0} is not a valid format!", MinimumLength = 3)]
+    public required string Password { get; set; }
+}
---
+++ Backend/BE-Authentication/Domain/Models/UserRegistrationModel.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Domain.DTO;
+
+public class UserRegistrationModel
+{
+    [Required(ErrorMessage = "The field {0} is required!")]
+    [EmailAddress(ErrorMessage = "The field {0} is not a valid format!")]
+    public required string Email { get; set; }
+
+    [Required(ErrorMessage = "The field {0} is required!")]
+    [StringLength(100, ErrorMessage = "The field {0} is not a valid format!", MinimumLength = 3)]
+    public required string Password { get; set; }
+
+    [Compare("Password", ErrorMessage = "The passwords must be the same")]
+    public required string ConfirmPassword { get; set; }
+}
