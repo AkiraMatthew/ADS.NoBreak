1dfa4995c399a82a189acb38ff2d5a15d24844a6
Mateus Henrique <akira.dev.math@gmail.com>
2025-05-04 22:40:57 -0300

    update

--- Backend/BE-Authentication/Services/Services/UserService.cs
+++ Backend/BE-Authentication/Services/Services/UserService.cs
@@ -1,19 +1,49 @@
 ﻿using Domain.DTO;
+using Domain.Entities;
 using Domain.Interfaces.Services;
 using Domain.Models;
+using Microsoft.AspNetCore.Identity;
 using System.Net;
 
 namespace Services.Services;
 
-public class UserService : IUserService
+public class UserService(UserManager<User> userManager) : IUserService
 {
-    public Task<Token> SignInAsync(NetworkCredential credentials)
+    public async Task<Token> SignInAsync(NetworkCredential credentials)
     {
-        throw new NotImplementedException();
+        try
+        {
+            
+        }
+        catch (Exception ex) { }
     }
 
-    public Task<string> SignUpAsync(NetworkCredential credentials, SignUpDTO signUp)
+    public async Task<string> SignUpAsync(NetworkCredential credentials, SignUpDTO signUp)
     {
-        throw new NotImplementedException();
+        try
+        {
+            var user = new User
+            {
+                Email = signUp.Email,
+                UserName = credentials.UserName.ToLower(),
+                UserCountry = signUp.Country
+            };
+
+            if(await Exists(user.UserName))
+                throw new Exception($"User already exists!");
+
+            var response = await userManager.CreateAsync(user, credentials.Password);
+            
+            if (!response.Succeeded)
+                throw new Exception($"Couldn't create a new user!");
+
+            var token = await 
+        }
+        catch (Exception ex) { }
+    }
+
+    private async Task<bool> Exists(string userName)
+    {
+        return await userManager.FindByNameAsync(userName) is not null;
     }
 }
